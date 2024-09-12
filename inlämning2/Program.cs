58aa89fe6f1005e51bc090d5af8594db317ccbf1
muja07001 <muja07001@E-PW05CGR9>
2024-09-12 14:26:12 +0200

    Add project files.

---
+++ inlämning2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inlämning2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Ändra bakgrundsfärg till blå
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            // Ändra textfärgen till vit
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("detta dator har alvarligt fel, ameen är bäst, ahla ameen");
+        }
+    }
+}
